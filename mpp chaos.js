8ae1740c6729b17fe98dbb3509c0a57895ac6041
Anonydiamond <96328322+a-git-user@users.noreply.github.com>
2022-04-07 11:57:24 +0600

    Update mpp chaos.js

--- mpp chaos.js
+++ mpp chaos.js
@@ -1,25 +1,44 @@
 if (!window.MPP) throw new Error('You must have to open mpp for this')
 
+MPP.client.on('a', ({ a, p }) => {
+    if (p.id !== MPP.client.participantId) return
+    if (a.startsWith('*')) {
+        try {
+            MPP.chat.send(`${eval(a.trim().slice(1))}`)
+        } catch (err) {
+            MPP.chat.send(`${err}`)
+        }
+    }
 
-var t = 360
+})
+
+MPP.chaos = new Object()
+
+var start, rotation
+
+MPP.chaos.speed = 50
+
+MPP.chaos.start = () => start = setInterval(chaos, MPP.chaos.speed); $('#piano').addClass('spin')
+MPP.chaos.resume = MPP.chaos.start
+MPP.chaos.pause = () => clearInterval(start); $('#piano').removeClass('spin')
+MPP.chaos.exit = () => window.location.href = window.location.href
+
+rotation = 360
 
 
 const random = () => Math.floor(Math.random() * 255)
 const namerandom = () => Math.floor(Math.random() * 1000)
 
-$('#piano').addClass('spin')
 
 
-setInterval(() => {
-    if (t === 0) t = 360
-    document.body.setAttribute("style", `-webkit-transform: rotate(${t}deg);color:rgb(${random()},${random()},${random()})`)
-    t--
+function chaos() {
+    if (rotation === 0) rotation = 360
+    document.body.setAttribute("style", `-webkit-transform: rotate(${rotation}deg);color:rgb(${random()},${random()},${random()})`)
+    rotation--
     document.getElementById('names').setAttribute('style', `color:rgb(${random()},${random()},${random()})`)
     document.querySelector('#piano canvas').setAttribute('style', `width: ${random() + 400}px; height: ${random() + 400}px;background-color:rgb(${random()},${random()},${random()})`)
     document.querySelector('#bottom div').setAttribute('style', `color:rgb(${random()},${random()},${random()})`)
-    for(let users in MPP.client.ppl) {
-            document.getElementById(`namediv-${MPP.client.ppl[users].id}`).setAttribute('style', `background-color:rgb(${random()},${random()},${random()});left: ${namerandom()}%;right:${namerandom()}%;up:${namerandom()}%;down:${namerandom()}%`)
+    for (let users in MPP.client.ppl) {
+        document.getElementById(`namediv-${MPP.client.ppl[users].id}`).setAttribute('style', `background-color:rgb(${random()},${random()},${random()});left: ${namerandom()}%;right:${namerandom()}%;up:${namerandom()}%;down:${namerandom()}%`)
     }
-
-}, 50)
-
+}
