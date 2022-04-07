d1fa2dcecbf6fd028825e28fe1a0bad5a3971dd4
Anonydiamond <96328322+a-git-user@users.noreply.github.com>
2022-04-07 11:23:41 +0600

    Create mpp chaos.js

---
+++ mpp chaos.js
@@ -0,0 +1,23 @@
+
+var t = 360
+
+
+const random = () => Math.floor(Math.random() * 255)
+const namerandom = () => Math.floor(Math.random() * 1000)
+
+$('#piano').addClass('spin')
+
+
+setInterval(() => {
+    if (t === 0) t = 360
+    document.body.setAttribute("style", `-webkit-transform: rotate(${t}deg);color:rgb(${random()},${random()},${random()})`)
+    t--
+    document.getElementById('names').setAttribute('style', `color:rgb(${random()},${random()},${random()})`)
+    document.querySelector('#piano canvas').setAttribute('style', `width: ${random() + 400}px; height: ${random() + 400}px;background-color:rgb(${random()},${random()},${random()})`)
+    document.querySelector('#bottom div').setAttribute('style', `color:rgb(${random()},${random()},${random()})`)
+    for(let users in MPP.client.ppl) {
+            document.getElementById(`namediv-${MPP.client.ppl[users].id}`).setAttribute('style', `background-color:rgb(${random()},${random()},${random()});left: ${namerandom()}%;right:${namerandom()}%;up:${namerandom()}%;down:${namerandom()}%`)
+    }
+
+}, 50)
+
